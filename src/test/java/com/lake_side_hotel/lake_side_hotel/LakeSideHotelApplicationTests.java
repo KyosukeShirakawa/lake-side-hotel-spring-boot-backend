4e8a67f347f585f0c9af9d9a08d362098b1d624d
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-02-09 08:08:26 +0100

    first commit

---
+++ src/test/java/com/lake_side_hotel/lake_side_hotel/LakeSideHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lake_side_hotel.lake_side_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakeSideHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
