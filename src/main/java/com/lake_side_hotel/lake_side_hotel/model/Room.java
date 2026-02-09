037b5307782b07cfa72e62cd119fd9f4b429b720
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-02-09 09:15:41 +0100

    defining properties of Room class

--- src/main/java/com/lake_side_hotel/lake_side_hotel/model/Room.java
+++ src/main/java/com/lake_side_hotel/lake_side_hotel/model/Room.java
@@ -1,4 +1,20 @@
 package com.lake_side_hotel.lake_side_hotel.model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+    private List<BookedRoom> bookings;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
+
+
+
 }
