70d9e211ea6677d370b58208e4e3529afca7c8ec
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-02-09 09:00:08 +0100

    creating class files

---
+++ src/main/java/com/lake_side_hotel/lake_side_hotel/controller/BookedRoomController.java
@@ -0,0 +1,4 @@
+package com.lake_side_hotel.lake_side_hotel.controller;
+
+public class BookedRoomController {
+}
---
+++ src/main/java/com/lake_side_hotel/lake_side_hotel/service/IRoomService.java
@@ -0,0 +1,4 @@
+package com.lake_side_hotel.lake_side_hotel.service;
+
+public interface IRoomService {
+}
