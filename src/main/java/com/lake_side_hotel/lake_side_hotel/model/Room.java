50a3bb7890dbc0c1b51dffc44f912ed352232b59
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-02-09 10:43:22 +0100

    associating Room class with BookedRoom and implementing addBooking method

--- src/main/java/com/lake_side_hotel/lake_side_hotel/model/Room.java
+++ src/main/java/com/lake_side_hotel/lake_side_hotel/model/Room.java
@@ -1,18 +1,49 @@
 package com.lake_side_hotel.lake_side_hotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.*;
+import org.apache.commons.lang3.RandomStringUtils;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String roomType;
     private BigDecimal roomPrice;
     private boolean isBooked = false;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room() {
         this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom booking) {
+        if(isBooked) {
+            return;
+        }
+        if(bookings == null) {
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
+
     }
 
 
